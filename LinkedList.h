4f96bd9e3e06af9a90937f1fb74b19667147644c
Domen122 <Maliszewski1222@gmail.com>
2016-12-01 23:51:32 +0100

    Add files via upload

--- LinkedList.h
+++ LinkedList.h
@@ -12,7 +12,7 @@ namespace aisdi
 template <typename Type>
 class LinkedList
 {
-public:
+private:
 
     struct Node
     {
@@ -64,21 +64,17 @@ public:
   }
   LinkedList(const LinkedList& other):First(nullptr), Last(nullptr), length(0)
   {
-            std::cout<<"WTF"<<std::endl;
-
-    /*for(auto ite=other.begin();ite!=other.end();++ite)
+    for(auto ite=other.begin();ite!=other.end();++ite)
       {
-        std::cout<<"WTF"<<std::endl;
         append(*ite);
-      }*/
-    (void)other;
-    //throw std::runtime_error("TODO");
+      }
   }
 
-  LinkedList(LinkedList&& other)
+  LinkedList(LinkedList&& other): First(other.First),Last(other.Last), length(other.length)
   {
-    (void)other;
-    throw std::runtime_error("TODO");
+    other.First=nullptr;
+    other.Last=nullptr;
+    other.length=0;
   }
   bool operator==(const LinkedList& other) const
   {
@@ -106,48 +102,80 @@ public:
     return false;
   }
   ~LinkedList()
-  {}
+  {
+    Node *act=First;
+    Node *tempo;
+    while(act!=nullptr)
+    {
+        tempo=act;
+        act=act->nextNode;
+        delete tempo;
+    }
+    First=nullptr;
+    Last=nullptr;
+  }
 
   LinkedList& operator=(const LinkedList& other)
   {
-    (void)other;
-    throw std::runtime_error("TODO");
+        Node *Actual = First;
+        Node *Tempo;
+        while(Actual!=nullptr)
+        {
+             Tempo = Actual;
+             Actual=Actual->nextNode;
+             delete Tempo;
+        }
+        First=nullptr;
+        Last=nullptr;
+        length=0;
+        for(auto it= other.begin();it!=other.end();++it)
+        {
+            append(*it);
+        }
+        return *this;
   }
 
   LinkedList& operator=(LinkedList&& other)
   {
-    (void)other;
-    throw std::runtime_error("TODO");
+    Node *Actual = First;
+        Node *Tempo;
+        while(Actual!=nullptr)
+        {
+             Tempo = Actual;
+             Actual=Actual->nextNode;
+             delete Tempo;
+        }
+        First=other.First;
+        Last=other.Last;
+        length=other.length;
+        other.First=nullptr;
+        other.Last=nullptr;
+        other.length=0;
+        return *this;
   }
 
   bool isEmpty() const
   {
     return(First==nullptr);
-    //throw std::runtime_error("TODO");
   }
 
   size_type getSize() const
   {
-    throw std::runtime_error("TODO");
+    return length;
   }
 
   void append(const Type& item)
   {
-    //std::cout<<length<<" ";
-
-
     Node* Newt= new Node(item);
      if(length==0)
      {
-        //std::cout<<"Pierwszy ele "<<item<<std::endl;
         First= Newt;
         Last=Newt;
-        //Newt->nextNode=nullptr;
-       // Newt->prevNode=nullptr;
+        Newt->nextNode=nullptr;
+        Newt->prevNode=nullptr;
      }
      else
      {
-        //std::cout<<"NiePierwszy ele "<<item<<std::endl;
         Last->nextNode=Newt;
         Newt->prevNode=Last;
         Newt->nextNode=nullptr;
@@ -155,73 +183,140 @@ public:
      }
      length++;
      if(First==nullptr)std::cout<<"Lul ferst jest nullptr"<<std::endl;
-     //std::cout<<"Po appendzie "<<length<<std::endl;
-    //(void)item;
-    //throw std::runtime_error("TODO");
   }
 
   void prepend(const Type& item)
   {
-    (void)item;
-    throw std::runtime_error("TODO");
+    Node *Newt = new Node(item);
+    if(length==0)
+    {
+        First=Newt;
+        Last=Newt;
+    }
+    else
+    {
+        First->prevNode=Newt;
+        Newt->nextNode=First;
+        First=Newt;
+    }
+    ++length;
   }
 
   void insert(const const_iterator& insertPosition, const Type& item)
   {
-    (void)insertPosition;
-    (void)item;
-    throw std::runtime_error("TODO");
+    if(insertPosition==begin()) prepend(item);
+    else if(insertPosition==end()) append(item);
+    else
+    {
+        Node *Newt = new Node(item);
+        Node *Tempo = insertPosition.GetNode();
+        Newt->nextNode=Tempo;
+        Newt->prevNode=Tempo->prevNode;
+        Tempo->prevNode->nextNode=Newt;
+        Tempo->prevNode=Newt;
+        ++length;
+    }
   }
 
   Type popFirst()
   {
-    throw std::runtime_error("TODO");
+        if(length==0) throw std::logic_error("Cannot pop from empty Collection");
+        Type Popped = First->Itemero;
+        Node *Tempo = First;
+        if(length==1)
+        {
+            First=nullptr;
+            Last=nullptr;
+        }
+        else
+        {
+            First=First->nextNode;
+            First->prevNode=nullptr;
+        }
+        --length;
+        delete Tempo;
+        return Popped;
   }
 
   Type popLast()
   {
-    throw std::runtime_error("TODO");
+        if(length==0) throw std::logic_error("Cannot pop from empty Collection");
+        Type Popped = Last->Itemero;
+        Node *Tempo = Last;
+        if(length==1)
+        {
+            First=nullptr;
+            Last=nullptr;
+        }
+        else
+        {
+            Last=Last->prevNode;
+            Last->nextNode=nullptr;
+        }
+        --length;
+        delete Tempo;
+        return Popped;
   }
 
   void erase(const const_iterator& possition)
   {
-    (void)possition;
-    throw std::runtime_error("TODO");
+      if(length==0) throw std::out_of_range("Cannot erase from empty Collection");
+      if(possition==end()) throw std::out_of_range("Cannot erase nullptr");
+      Node *Tempo = possition.GetNode();
+      if(length==1)
+      {
+        First=nullptr;
+        Last=nullptr;
+      }
+      else if(possition==begin())
+      {
+        First=First->nextNode;
+        First->prevNode=nullptr;
+      }
+      else if(Tempo==Last)
+      {
+        Last=Last->prevNode;
+        Last->nextNode=nullptr;
+      }
+      else
+      {
+        Tempo->prevNode->nextNode=Tempo->nextNode;
+        Tempo->nextNode->prevNode=Tempo->prevNode;
+      }
+      delete Tempo;
+      --length;
+
   }
 
   void erase(const const_iterator& firstIncluded, const const_iterator& lastExcluded)
   {
-    (void)firstIncluded;
-    (void)lastExcluded;
-    throw std::runtime_error("TODO");
+    if(firstIncluded==lastExcluded)return;
+    for( auto it=firstIncluded+1;it!=lastExcluded;++it) erase(it-1);
+    erase(lastExcluded-1);
   }
 
   iterator begin()
   {
     Iterator ite(*this,First);
     return ite;
-    //throw std::runtime_error("TODO");
   }
 
   iterator end()
   {
     Iterator ite(*this, nullptr);
     return ite;
-    //throw std::runtime_error("TODO");
   }
 
   const_iterator cbegin() const
   {
     ConstIterator ite(*this,First);
     return ite;
-    //throw std::runtime_error("TODO");
   }
 
   const_iterator cend() const
   {
     ConstIterator ite(*this, nullptr);
     return ite;
-    //throw std::runtime_error("TODO");
   }
 
   const_iterator begin() const
@@ -239,8 +334,8 @@ public:
 template <typename Type>
 class LinkedList<Type>::ConstIterator
 {
-  public:
-  const LinkedList<Type> Lyst;
+  protected:
+  const LinkedList<Type> &Lyst;
   Node* point;
 
 
@@ -257,10 +352,14 @@ public:
   {}
     ConstIterator(const ConstIterator& other): Lyst(other.Lyst), point(other.point)
     {}
+    Node * GetNode() const
+    {
+        return point;
+    }
   reference operator*() const
   {
+    if(*this==Lyst.end())throw std::out_of_range("Iterator wskazuje na end()");
     return point->Itemero;
-    //throw std::runtime_error("TODO");
   }
 
   ConstIterator& operator++()
@@ -268,7 +367,6 @@ public:
     if(*this == Lyst.end())throw std::out_of_range("Cannot increment out of the List");
     point=point->nextNode;
     return *this;
-    //throw std::runtime_error("TODO");
   }
 
   ConstIterator operator++(int)
@@ -277,19 +375,17 @@ public:
     auto PreviousPosition = *this;
     point=point->nextNode;
     return PreviousPosition;
-    //throw std::runtime_error("TODO");
   }
 
   ConstIterator& operator--()
   {
-    //if(*this == Lyst.begin())throw std::out_of_range("Cannot decrement out of the List");
-   /* else*/ if(*this == Lyst.end())
+    if(*this == Lyst.begin())throw std::out_of_range("Cannot decrement out of the List");
+    else if(*this == Lyst.end())
     {
         point=Lyst.Last;
     }
     else point=point->prevNode;
     return *this;
-    //throw std::runtime_error("TODO");
   }
 
   ConstIterator operator--(int)
@@ -299,32 +395,42 @@ public:
     else if(*this == Lyst.end()) point=Lyst.Last;
     else point=point->prevNode;
     return PreviousPosition;
-    //throw std::runtime_error("TODO");
   }
 
   ConstIterator operator+(difference_type d) const
   {
-    (void)d;
-    throw std::runtime_error("TODO");
+     auto result = *this;
+     for(int i=0;i<d;++i)
+     {
+        result.point=result.point->nextNode;
+     }
+     return result;
   }
 
   ConstIterator operator-(difference_type d) const
   {
-    (void)d;
-    throw std::runtime_error("TODO");
+     auto result = *this;
+     if(*this == Lyst.end())
+     {
+        result.point=result.Lyst.Last;
+        --d;
+     }
+     for(int i=0;i<d;++i)
+     {
+        result.point=result.point->prevNode;
+     }
+     return result;
   }
 
   bool operator==(const ConstIterator& other) const
   {
     if(Lyst!=other.Lyst)std::cout<<"lellelelelelelelelel"<<std::endl;
     return(point == other.point && Lyst == other.Lyst);
-    //throw std::runtime_error("TODO");
   }
 
   bool operator!=(const ConstIterator& other) const
   {
      return(point!=other.point && Lyst==other.Lyst);
-    //throw std::runtime_error("TODO");
   }
 };
 
